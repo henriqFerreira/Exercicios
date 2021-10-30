d3fec51e1be48a742a5e1b77e110b51091923c02
Henrique Neto <henriqueneto1912@gmail.com>
2021-10-29 22:00:05 -0300

    Exercicios 1 e 2, python

---
+++ Python/2. Welcome!/main.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de
+# boas-vindas para ela:
+# 
+# Ex:
+# Qual o seu nome? João da Silva
+# Olá, João da Silva, é um prazer te conhecer!
+nome = input("Qual o seu nome? ")
+print("Olá, " + nome + ", é um prazer te conhecer!")
