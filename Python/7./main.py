241218ad18bcf38ea179975bf992039568b3a94f
Henrique Neto <henriqueneto1912@gmail.com>
2021-12-31 17:07:27 -0300

    Exerc. 7

---
+++ Python/7./main.py
@@ -0,0 +1,10 @@
+# 7) Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a
+# sua terça parte.
+# 
+# Ex:
+# Digite um número: 3.5
+# O dobro de 3.5 é 7.0
+# A terça parte de 3.5 é 1.16666
+num = float(input("Digite um número: "))
+print("O dobro de " + str(num) + " é " + str(num * 2))
+print("A terça parte de " + str(num) + " é " + str(num / 3))
